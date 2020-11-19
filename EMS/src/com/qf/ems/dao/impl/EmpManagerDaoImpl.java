d586b270cbbea05e5a5730516ac3632de71bb59d
Techoc <543938801@qq,com>
2020-11-19 18:02:50 +0800

    2020-11-19 完成Dbutils工具类

---
+++ EMS/src/com/qf/ems/dao/impl/EmpManagerDaoImpl.java
@@ -0,0 +1,11 @@
+package com.qf.ems.dao.impl;
+
+import com.qf.ems.dao.EmpManagerDao;
+import com.qf.ems.entity.EmpManager;
+
+public class EmpManagerDaoImpl implements EmpManagerDao {
+    @Override
+    public EmpManager select(String username) {
+        return null;
+    }
+}
